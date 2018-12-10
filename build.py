90c77b9861a0c69d0ab9825db8171d8eb4cc2a5b
minus <minus@mnus.de>
2018-12-10 21:39:26 +0100

    Refactor build.py

--- build.py
+++ build.py
@@ -1,45 +1,58 @@
 #!/usr/bin/env python3
-# vim: set tabstop=4 noexpandtab shiftwidth=4:
+
+# Requires Python 3.4+ and misaka
 
 import misaka as m
-import os
 import sys
-import shutil
 import re
-import functools
+from pathlib import Path
+import shutil
+from argparse import ArgumentParser
+from enum import Enum
 
-BUILDDIR = "html"
 
-BUILDTYPE = "local"
-if len(sys.argv) > 1:
-	BUILDTYPE = sys.argv[1]
+def main():
+    p = ArgumentParser()
+    p.add_argument('-t', '--build-type', type=BuildType,
+            choices=tuple(BuildType), default=BuildType.LOCAL.value)
+    p.add_argument('source', type=Path)
+    p.add_argument('target', type=Path)
+    args = p.parse_args()
 
-if os.path.exists(BUILDDIR):
-	shutil.rmtree(BUILDDIR)
+    target = args.target.with_suffix('.new')
+    compile_docs(args.source, target, args.build_type)
+    shutil.rmtree(args.target)
+    target.replace(args.target)
 
-os.mkdir(BUILDDIR)
 
-@functools.lru_cache()
-def mkdir_cached(dir):
-	if not os.path.exists(dir):
-		os.makedirs(dir)
+class BuildType(Enum):
+    LOCAL = 'local'
+    TEEWORLDS_COM = 'teeworlds.com'
 
-for root, dirs, files in os.walk("."):
-	for fn in files:
-		if fn.endswith(".md"):
-			input_path = os.path.join(root, fn)
-			output_dir = os.path.join(BUILDDIR, root)
-			output_path = os.path.join(output_dir, fn[:-3]+".html")
-			mkdir_cached(output_dir)
-			print("Processing {}...".format(input_path), file=sys.stderr)
-			with open(input_path, "r") as inp, open(output_path, "w") as out:
-				data = inp.read()
-				def link_patcher(match):
-					if BUILDTYPE == "teeworlds.com":
-						path = os.path.normpath(os.path.join(root, match.group(2)))
-						return "[{}]({})".format(match.group(1), "/?page=docs&wiki="+path)
-					else:
-						return "[{}]({})".format(match.group(1), match.group(2)+".html")
-				data = re.sub(r"\[([^\]]+)]\(\s*([^\)]+)\.md\s*\)", link_patcher, data)
-				html = m.html(data, extensions=m.EXT_TABLES | m.EXT_FENCED_CODE, render_flags=m.HTML_USE_XHTML)
-				out.write(html)
+
+def compile_docs(source, target, build_type):
+    for input_path in source.glob('**/*.md'):
+        output_dir = (target / input_path.relative_to(source)).parent
+        output_dir.mkdir(exist_ok=True, parents=True)
+        output_path = output_dir / (input_path.with_suffix('.html').name)
+        print("Processing {}...".format(input_path), file=sys.stderr)
+
+        def link_patcher(match):
+            if build_type == BuildType.TEEWORLDS_COM:
+                path = (input_path.parent / match.group(2)).resolve().relative_to(source)
+                return "[{}]({})".format(match.group(1), '/?page=docs&wiki=' + str(path))
+            elif build_type == BuildType.LOCAL:
+                return "[{}]({})".format(match.group(1), match.group(2) + '.html')
+            else:
+                raise ValueError("Unsupported BuildType")
+
+        with input_path.open() as inp, output_path.open('w') as out:
+            data = inp.read()
+            data = re.sub(r'\[([^\]]+)]\(\s*([^\)]+)\.md\s*\)', link_patcher, data)
+            html = m.html(data, extensions=m.EXT_TABLES | m.EXT_FENCED_CODE,
+                    render_flags=m.HTML_USE_XHTML)
+            out.write(html)
+
+
+if __name__ == '__main__':
+    main()
